eff0e295d3d2835c1f8a9d69393040052e9800a0
moldamolda <moldamolda@local>
2026-07-20 21:00:20 +0200

    aefwef

---
+++ rebuild.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
+ln -sfn "$DIR" ~/.dotfiles
+nix run home-manager -- switch --flake ~/.dotfiles#molda
+
+cd "$DIR"
+git add .
+git commit -m "${1:-update}"
+git push

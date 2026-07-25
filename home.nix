0ed4bbe2b89e3058bf199c918551f73b39faafcf
moldamolda <moldamolda@local>
2026-07-25 21:23:58 +0200

    move repo into WSL filesystem, add bootstrap.sh and wezterm config

--- home.nix
+++ home.nix
@@ -5,7 +5,7 @@
   home.stateVersion = "24.05";
 
   nixpkgs.config.allowUnfree = true;
-  fonts.fontconfig.enable = true
+  fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
     ripgrep

b18f4aba3e6520999252ddf905b1a82d0fb4d540
moldamolda <moldamolda@local>
2026-07-27 19:40:35 +0200

    add title bar to wezterm for window dragging

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -8,13 +8,6 @@ config.font_size = 15.0
 config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations = "RESIZE"
-config.mouse_bindings = {
-  {
-    event = { Drag = { streak = 1, button = "Left" } },
-    mods = "ALT",
-    action = wezterm.action.StartWindowDrag,
-  },
-}
+config.window_decorations = "TITLE | RESIZE"
 
 return config

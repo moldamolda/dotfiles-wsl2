f8849747f0560e7fce33adbac3ab07202760b76c
moldamolda <moldamolda@local>
2026-07-27 20:04:28 +0200

    use a color scheme available in current wezterm version

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "rose-pine-moon"
+config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 0.8

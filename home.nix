bd2497e3b8cdf7ed94d6d0082730f91103022fe2
moldamolda <moldamolda@local>
2026-07-27 19:23:05 +0200

    use ~/.dotfiles symlink path instead of hardcoded repo folder name

--- home.nix
+++ home.nix
@@ -1,12 +1,13 @@
 { pkgs, config, user, ... }:
+let
+  dotfiles = "${config.home.homeDirectory}/.dotfiles";
+in
 {
   home.username = user;
   home.homeDirectory = "/home/${user}";
   home.stateVersion = "24.05";
-
   nixpkgs.config.allowUnfree = true;
   fonts.fontconfig.enable = true;
-  
   home.packages = with pkgs; [
     ripgrep
     fd
@@ -17,14 +18,11 @@
     claude-code
     nerd-fonts.hack
   ];
-
   home.file.".config/wezterm".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/molda_dotfiles/home/.config/wezterm";
-
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
   home.sessionVariables = {
     EDITOR = "nvim";
   };
-
   programs.zsh = {
     enable = true;
     initExtra = ''

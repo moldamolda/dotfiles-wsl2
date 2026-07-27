81fcaf4544a3ee5ddd52bbb8b8f1abf97ff526f7
moldamolda <moldamolda@local>
2026-07-27 19:38:10 +0200

    enable window dragging in wezterm

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -9,5 +9,12 @@ config.window_background_opacity = 0.8
 config.macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
+config.mouse_bindings = {
+  {
+    event = { Drag = { streak = 1, button = "Left" } },
+    mods = "ALT",
+    action = wezterm.action.StartWindowDrag,
+  },
+}
 
 return config

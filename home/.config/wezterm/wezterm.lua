ee13be044600d53366b0829aa01b3c687e3fe448
moldamolda <moldamolda@local>
2026-07-29 20:46:52 +0200

    update

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -7,6 +7,8 @@ config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
 config.window_background_opacity = 1.0 
 macos_window_background_blur = 50
+config.initial_cols = 120
+config.initial_rows = 35
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "TITLE | RESIZE"
 

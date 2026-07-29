ab2d81e963125056f4d181bd47f07698b24d790a
moldamolda <moldamolda@local>
2026-07-29 20:41:26 +0200

    no

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -5,8 +5,8 @@ local config = wezterm.config_builder()
 config.color_scheme = "Tokyo Night"
 config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 15.0
-config.window_background_opacity = 0.8
-config.macos_window_background_blur = 50
+config.window_background_opacity = 1.0 
+macos_window_background_blur = 50
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "TITLE | RESIZE"
 
